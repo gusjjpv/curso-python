4097257d00110ca1f4adaaa4557eb3a977e67968
gus <111452823+gusjjpv@users.noreply.github.com>
2023-10-01 17:45:30 -0300

    Add files via upload
    Peça ao usuário para digitar três números inteiros e imprima a soma deles

---
+++ Curso python/exercicios/soma inteiros.py
@@ -0,0 +1,5 @@
+num1 = int(input('informe um númeor inteiro: '))
+num2 = int(input('informe um númeor inteiro: '))
+num3 = int(input('informe um númeor inteiro: '))
+soma = num1 + num2 + num3
+print(f'a soma dos numeros informados é {soma}')
