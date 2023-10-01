ecc71080cb00c00a238db7ae505dd6f6458a0df8
gus <111452823+gusjjpv@users.noreply.github.com>
2023-10-01 17:46:16 -0300

    Add files via upload
    Leia um numero real e imprima a quinta parte dele

---
+++ Curso python/exercicios/quinta parte num float.py
@@ -0,0 +1,8 @@
+# Lê um número real do usuário
+numero_real = float(input("Digite um número real: "))
+
+# Calcula a quinta parte do número
+quinta_parte = numero_real / 5
+
+# Imprime a quinta parte do número
+print("A quinta parte do número que você digitou é:", quinta_parte)
