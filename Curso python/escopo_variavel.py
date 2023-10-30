8d5235cb89f53f2443402a6094a183d650608921
Joao Gustavo <joaogustavolima084@gmail.com>
2023-10-30 14:25:37 -0300

    escopo de variavel

---
+++ Curso python/escopo_variavel.py
@@ -0,0 +1,13 @@
+"""
+Escopo de variavel
+
+Dois casos de escopo:
+1 - Globais
+    são reconhecidas em todo o programa
+2 - Locais
+    são reconhecidas somente no bloco que foram declaradas
+
+Python é uma linguagem de tipagem dinamica. isso significa que ao declararmos uma variavel, nós não colocamos o tipo de dado dela.
+este tipo inferido ao atribuimos o valor a mesma
+
+"""
