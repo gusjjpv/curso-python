fb70f8708491a1815eae9cee8d46dcf78fe7a626
Joao Gustavo <joaogustavolima084@gmail.com>
2023-10-30 14:26:00 -0300

    estruturas logicas

---
+++ Curso python/estru_logicas_or_and_is_not.py
@@ -0,0 +1,20 @@
+"""
+Estruturas Logicas:
+    and(e), or(ou), not(não), is(é)
+    
+operadores unarios:
+    -not
+operadores binarios:
+    - and, or, is
+    
+Para o 'and', ambos os valores precisam ser True
+Para o 'or', um ou outro valor precisa ser True
+Para o 'not', o valor do booleano é invertido, ou seja, se for True, vira False, se for False vira True
+Para o 'is', ovalor é comparado com um segundo.
+"""
+
+ativo = False
+logado = True
+
+#ativo é falso?
+print(ativo is False)   
