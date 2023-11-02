4efb8a2dbee1586b2222c46ab60e064015a0ef0b
Joao Gustavo <joaogustavolima084@gmail.com>
2023-11-02 16:03:50 -0300

    Create loop_while.py

---
+++ Curso python/loop_while.py
@@ -0,0 +1,27 @@
+"""
+Loop while
+
+forma geral
+
+while expressão_booleana:
+    //execução do loop
+    
+bloco do while sera repetida enquanto a expressão booleana for verdadeira
+
+#Exemplo 1
+
+numero = 1 
+
+while numero < 10:
+    print(numero)
+    # numero = numero + 1
+
+#OBS: Em um loop while, é importante que cuidamos do criterio de parada para não causar um loop infinito.
+
+resposta = ''
+
+while resposta != 'sim':
+    resposta = input('Já acabou jessica?')
+
+#NÃO temos do while no python
+"""
