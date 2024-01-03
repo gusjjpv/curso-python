7809e864c952c80f5258d92c983644d71a704900
Joao Gustavo <joaogustavolima084@gmail.com>
2024-01-03 15:45:08 -0300

    Create 2.inverterSubstituir.py

---
+++ Curso python/Exercícios de prática/seção 04/2.inverterSubstituir.py
@@ -0,0 +1,8 @@
+frase = 'O São Paulo é gigante, gigante'
+print(frase)
+
+fraseNova = frase.replace('gigante', 'O melhor')
+print(fraseNova)
+
+fraseInvertida = fraseNova[::-1]
+print(fraseInvertida)
