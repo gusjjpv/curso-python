6ba2e678a4e043cd31b45b5cbaa77a2590d13fc1
Joao Gustavo <joaogustavolima084@gmail.com>
2024-01-03 15:45:01 -0300

    Update 6-tipo_string.py

--- Curso python/Anotações de estudo/6-tipo_string.py
+++ Curso python/Anotações de estudo/6-tipo_string.py
@@ -8,7 +8,7 @@ Em Python, um dado é considerado do tipo string sempre que:
 - Estiver entre aspas simples triplas -> '''uma string''', '''234''', '''a'''
 - Estiver entre aspas duplas triplas
 
-Se voce abrir asa aspas com uma simples, voce tem que finalizar com uma simples e assim por diante
+Se voce abrir as aspas com uma simples, voce tem que finalizar com uma simples e assim por diante
 """
 
 nome = 'gus'
@@ -19,7 +19,7 @@ print(frase)
 print(type(frase))
 
 # O mais comum é o aspas simples
-# Para usar uma aspas imples em uma string, usa uma aspas duplas, se for usar uma
+# Para usar uma aspas simples em uma string, usa uma aspas duplas, se for usar uma
 # aspas duplas, usa aspas simples/duplas triplas
 
 bar = "Gina's Bar"
@@ -37,6 +37,7 @@ gus"""
 print(nome)
 
 """Funções legais para String"""
+
 # Colocar tudo em maiusculo
 
 nome = 'João Gustavo'
@@ -50,14 +51,18 @@ print(nome.lower())
 # Colocar em um lista/array
 
 sp = 'São Paulo o maior do Brasil'
-print(nome.split())
+print(sp.split())
 
 # Para imprimir uma parte da frase, vamos usar 'arrays', chamamos isso de SLICE DE STRING
-print(sp[0:9])
 # Nesse exemplos vamos do indice 0(primeira letra), ate o indice 9
-print(sp[10:27])
+print(sp[0:9])
+
 # Imprime 'O maior do Brasil', esta começando do indice 10 e vai ate o 27
+print(sp[10:27])
+
+
 # Podemos usar o split para tranformar em uma lista e usar o slice de string
+
 sp = 'São Paulo'
 print(sp.split()[1])
 
@@ -70,7 +75,8 @@ nome = 'sao paulo'
 
 print(nome.upper()[::-1])
 print('1', sp[2::-1])
-# trocar letras
 
+# trocar letras
+               #onde tiver 's' vai ser substituido por 'j'     
 print(nome.replace('s', 'j'))
 # em sao paulo temos dois 'a', se substituimos o 'a' por outra letra, vai ser trocada todas as letras 'a'
