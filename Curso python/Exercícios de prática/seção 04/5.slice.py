fe316ae3a22692cd84cfa4a91d9f562e327874d6
Joao Gustavo <joaogustavolima084@gmail.com>
2024-01-03 15:46:23 -0300

    Create 5.slice.py

---
+++ Curso python/Exercícios de prática/seção 04/5.slice.py
@@ -0,0 +1,12 @@
+frase = 'oi, tudo bem'
+
+frase5carac =frase[0:5]
+print(frase5carac)
+
+fraseResto = frase[5:]
+
+print(fraseResto)
+
+fraseConcatenada = frase5carac + fraseResto
+
+print('frase concatenada: ',fraseConcatenada)
