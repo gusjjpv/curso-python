33885097ce0debb4012a84420d34385242b8d856
gusDev <joaogustavolima084@gmail.com>
2024-01-11 15:34:04 -0300

    Update 22.deque.py

--- Curso python/Anotações de estudo/22.deque.py
+++ Curso python/Anotações de estudo/22.deque.py
@@ -1,3 +1,33 @@
 """
-Mo
+Modulo Collections - Deque
+
+podemos dizer que o deque é uma lista de alta performace.
+
 """
+
+# import
+
+from collections import deque
+
+# criando deques
+
+deq = deque('geek')
+
+print(deq)
+
+#adiionando elementos no deque
+
+deq.append('y') # adiciona no final, igual em listas
+print(deq)
+
+deq.appendleft('k') # adiciona no começo 
+print(deq)
+
+# remover elementos
+
+print(deq.pop()) # remove e retorna o ultimo elemento
+print(deq)
+
+print(deq.popleft()) #remove e retorna o primeiro elemento
+
+print(deq)
