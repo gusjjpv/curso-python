3a2b15d45c11642a9a6c146df447cc31efc27192
gusDev <joaogustavolima084@gmail.com>
2024-01-21 14:57:25 -0300

    Create 2.data.py

---
+++ Curso python/Exercícios de prática/seção 08/2.data.py
@@ -0,0 +1,8 @@
+def formatar_data(dia,mes,ano):
+    print(f'{dia} de {mes} de {ano}')
+
+dia = int(input('Digite o dia: '))
+mes = int(input('Digite o mes:'))
+ano = int(input('Digite o ano:'))
+
+formatar_data(dia,mes,ano)
