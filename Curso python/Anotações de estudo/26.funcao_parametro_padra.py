779ea098b3baa9d5425b0c089afc00a1ad828952
gusDev <joaogustavolima084@gmail.com>
2024-01-26 22:22:11 -0300

    Update 26.funcao_parametro_padra.py

--- Curso python/Anotações de estudo/26.funcao_parametro_padra.py
+++ Curso python/Anotações de estudo/26.funcao_parametro_padra.py
@@ -17,5 +17,40 @@ OBS:
 def teste(num=2, potencia):
     return num ** potencia
 
+Todos os tipos de dados podem ser usados como default para parametros
 
+# exemplo 
+
+def soma(num1,num2):
+    return num1 + num2
+
+def mat(num1,num2,fun=soma):
+    return fun(num1,num2)
+
+def subtracao(num1,num2):
+    return num1 - num2
+
+print(mat(2,6))
+print(mat(4,7,subtracao))
+
+# ATENÇÃO com variaveis globais(se possivel evitar)
+
+total = 0
+def incremento():
+    global total # avisando que queremos utilizar a variavel global
+# se n utiliza o global, ira dar erro, pos isso na função é bom evitar o uso de variaveis globais    
+    total = total + 1
+    return total
+
+# Podemos ter funções que são declaradas dentro de funções, e tambem tem uma forma especial de escopo de variavel
+
+def fora():
+    contador = 0
+    #para usar uma variavel de uma função em uma função que esta dentro usamos o nonlocal para indicar 
+    def dentro():
+       nonlocal contador
+       contador = contador + 1
+       return contador
+    return dentro()
+#só podemos usar a função dentro() chamando a função fora()
 """
