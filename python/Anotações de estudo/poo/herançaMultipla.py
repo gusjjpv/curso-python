f9ede6a515d89d65b8ca24be97bdbe1a5df98c5c
gusDev <joaogustavolima084@gmail.com>
2024-04-27 15:39:50 -0300

    heranca multipla

--- python/Anotações de estudo/poo/herançaMultipla.py
+++ python/Anotações de estudo/poo/herançaMultipla.py
@@ -1,3 +1,37 @@
-"""
-    herança multipla
-"""
+class Animal:
+    def __init__(self, nro_patas):
+        self.nro_patas = nro_patas
+    
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+class Mamifero(Animal):
+    def __init__(self, cor_pelo, **kw):
+        self.cor_pelo = cor_pelo
+        super().__init__(**kw)
+
+class Ave(Animal):
+    def __init__(self, cor_bico, **kw):
+        self.cor_bico = cor_bico
+        super().__init__(**kw)
+
+class Gato(Mamifero):
+    pass
+
+
+class FalarMixin:
+    def falar(self):
+        return 'Falar'
+
+
+class Ornitorrinco(Mamifero ,Ave, FalarMixin):
+    pass
+
+gato = Gato(nro_patas=4, cor_pelo='preto')
+
+print(gato)
+
+ornitorrinco = Ornitorrinco(nro_patas=4, cor_pelo='marrom', cor_bico='laranja')
+print(ornitorrinco)
+
+print(ornitorrinco.falar())
